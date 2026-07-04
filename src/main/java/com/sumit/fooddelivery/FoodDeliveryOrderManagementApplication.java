bf92af61695b15e1102af7661699ee78592ce842
sumitjha480 <sumitjha480@gmail.com>
2026-07-05 03:47:36 +0530

    spring initialiser project creation

---
+++ src/main/java/com/sumit/fooddelivery/FoodDeliveryOrderManagementApplication.java
@@ -0,0 +1,13 @@
+package com.sumit.fooddelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryOrderManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryOrderManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sumit/fooddelivery/FoodDeliveryOrderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumit.fooddelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryOrderManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
